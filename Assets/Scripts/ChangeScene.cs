a8c45a4f042ecd82d111f8cba5f85671b545f451
Kevin Yamel Díaz Pérez <kevindiaz@ceroca.cat>
2025-10-23 19:14:58 +0200

    ChangeScene
    Agregados los cambio de escenas

--- Assets/Scripts/ChangeScene.cs
+++ Assets/Scripts/ChangeScene.cs
@@ -2,8 +2,27 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class ChangeScene : MonoBehaviour
 {
-  public void MoveToNeonLandInGame()
+    public void MoveToNeonLandInGame()
   {
     UnityEngine.SceneManagement.SceneManager.LoadScene("NeonLand_InGame");
     }
+
+    public void MoveFromNeoLand_InGameToFabric_InGame()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Fabric_InGame");
+    }
+    public void MoveFromFabric_InGameToInGameEnd()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("InGame_End");
+    }
+    public void MoveFromInGameEndToMenu_Start()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Menu_Start");
+    }
+    public void MoveToMenu_InGame()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Menu_InGame");
+    }
+
+
 }
